fbc4176989aea20de95fefcc2f49737fb79b6fbd
Wautumn <2283149823@qq.com>
2017-06-02 23:14:46 +0800

    服务器

--- serverlayer.cpp
+++ serverlayer.cpp
@@ -25,7 +25,7 @@ bool serverlayer::init()
 	menuItem->setPosition(Vec2(winSize.width *0.8, winSize.height*0.2));
 
 	MenuItemLabel *menuItem_2 = MenuItemLabel::create(Label::createWithSystemFont("client", "", 30), CC_CALLBACK_1(serverlayer::menucallback, this));
-	menuItem_2->setColor(Color3B(0, 0, 0));
+	menuItem_2->setColor(Color3B(0, 0, 255));
 	menuItem_2->setTag(102);
 	menuItem_2->setPosition(Vec2(winSize.width *0.8, winSize.height*0.1));
 
